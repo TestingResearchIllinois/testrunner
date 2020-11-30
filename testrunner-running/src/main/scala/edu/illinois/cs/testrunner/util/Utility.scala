6d1f6ad75b922f52ca9d1df58a75238a97df1c6b
luoos <junluo2@illinois.edu>
2020-11-29 21:01:13 -0500

    Get All Methods for JUnit 5 Class (#11)
    * Get All Methods for JUnit 5 Class
    
    Unlike JUnit 4 tests, which must be public, JUnit 5 tests can be
    non-public (package-private). While getMethods function only
    returns public methods, leading to failure of finding JUnit 5 tests.
    
    Change getMethods to getDeclaredMethods so that we can get all
    methods of a test class.
    
    Also, call getDeclaredMethods of superclass iteratively to
    get all the inherited methods.
    
    * Skip Unqualified JUnit 5 Nested Test Class
    
    A nested test class should
    (1) be non-static
    (2) have @Nested annotation
    
    * Exclude Disabled Test Classes

--- testrunner-running/src/main/scala/edu/illinois/cs/testrunner/util/Utility.scala
+++ testrunner-running/src/main/scala/edu/illinois/cs/testrunner/util/Utility.scala
@@ -1,9 +1,29 @@
 package edu.illinois.cs.testrunner.util
 
+import java.lang.reflect.Method;
 import java.time.format.DateTimeFormatter
 import java.time.{Instant, ZoneId}
+import scala.collection.mutable.HashSet
+import scala.collection.mutable.ListBuffer
 
 object Utility {
     def timestamp(): String =
         DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss").format(Instant.now().atZone(ZoneId.systemDefault()))
+
+    def getAllMethods(clz: Class[_]): Stream[Method] = {
+        var curClz = clz;
+        val methods = new ListBuffer[Method]()
+        val nameSet = HashSet[String]()
+        while (curClz != null) {
+            for (m <- curClz.getDeclaredMethods) {
+                if (!nameSet.contains(m.getName)) {
+                    // exclude override method
+                    methods.append(m)
+                    nameSet.add(m.getName)
+                }
+            }
+            curClz = curClz.getSuperclass
+        }
+        methods.toStream
+    }
 }
